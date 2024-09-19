c40d4e9a85b5715fb45ef9ee63553bdc1c33a98b
Cody Eksal <ceksal@wooplagaming.com>
2024-09-19 20:42:21 -0300

    Do not strip absolute path for dart2js apps

--- dart/lib/src/sentry_stack_trace_factory.dart
+++ dart/lib/src/sentry_stack_trace_factory.dart
@@ -129,7 +129,7 @@ class SentryStackTraceFactory {
     final platform = _options.platform.isWeb ? 'javascript' : 'dart';
     final fileName =
         frame.uri.pathSegments.isNotEmpty ? frame.uri.pathSegments.last : null;
-    final abs = '$eventOrigin${_absolutePathForCrashReport(frame)}';
+    final abs = _absolutePathForCrashReport(frame);
 
     var sentryStackFrame = SentryStackFrame(
       absPath: abs,
@@ -164,6 +164,7 @@ class SentryStackTraceFactory {
   String _absolutePathForCrashReport(Frame frame) {
     if (frame.uri.scheme != 'dart' &&
         frame.uri.scheme != 'package' &&
+        !frame.uri.scheme.startsWith('http') &&
         frame.uri.pathSegments.isNotEmpty) {
       return frame.uri.pathSegments.last;
     }
